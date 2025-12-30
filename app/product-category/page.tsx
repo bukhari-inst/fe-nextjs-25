e24a443654eb2f3e994c82098215ce5d5dc5d4f3
Bukhari_Inst <bukhari414647@gmail.com>
2025-12-30 08:14:23 +0700

    feat: Add new product category page with initial data fetching.

--- app/product-category/page.tsx
+++ app/product-category/page.tsx
@@ -1,7 +1,17 @@
 import Layout from '@/components/ui/Layout';
-import React from 'react';
+import { service } from '@/services/services';
+import React, { useEffect } from 'react';
 
 export default function Page() {
+  const getData = async () => {
+    const response = await service('product-category');
+    console.log(response);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <Layout>
       <h1 className="text-black">Product Category</h1>
